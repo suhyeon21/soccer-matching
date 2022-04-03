76819320a80287f51883f811dbfbec3aecff6e4c
suhyeon <suhyeon.kan@gmail.com>
2022-04-03 23:34:54 +0900

    feat: json 파일 추가하고 데이터 가져오기

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+// Fetch the items from the JSON file
+function loadItems() {
+  return fetch("data/data.json")
+    .then((response) => response.json())
+    .then((json) => json.matches);
+}
+// Update the list with the given items
+function displayItems(matches) {
+  const container = document.querySelector(".lists");
+  container.innerHTML = matches
+    .map((match) => createHTMLString(match))
+    .join("");
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(match) {
+  return `
+    <li class="matches">
+        <span class="match time">${match.time}</span>
+        <span class="match title">${match.title}</span> 
+        <span class="match option">${match.option}</span> 
+        <span class="match status">${match.status}</span>
+    </li>
+    `;
+}
+
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const banners = document.querySelector(".banners");
@@ -10,3 +36,9 @@ button.addEventListener("click", function () {
   banners.style.transform = "translate(" + -100 * counter + "em)";
   console.log(counter);
 });
+
+loadItems()
+  .then((matches) => {
+    displayItems(matches);
+  })
+  .catch(console.log);
